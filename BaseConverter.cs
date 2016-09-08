7732bbdffb5df91b8ffd1ca7541b0c34ab524eb7
Ivy Chang <ivycwq@icloud.com>
2016-09-08 13:51:24 -0700

    calculating conversions

--- BaseConverter.cs
+++ BaseConverter.cs
@@ -3,8 +3,8 @@
 namespace BaseConverter {
     public class BaseConverter {
         public int Number {get; set; }
-        public string FromBase {get; set; }
-        public string ToBase {get; set; }
+        public string FromBase {get{ return FromBase; } set{ FromBase =  value.ToLower(); } }
+        public string ToBase {get{ return ToBase; } set{ ToBase = value.ToLower(); } }
         public void Init() {
             Number = 0;
             FromBase = "Decimal";
@@ -16,44 +16,84 @@ namespace BaseConverter {
         public void ResetBaseConverter() {
             Init();
         }
-        public int DecimalToBinary() {
-
+        public string DecimalToBinary() {
+            return Convert.ToString(Number, 2);
         }
         public int BinaryToDecimal() {
-
+            return Convert.ToInt32(Convert.ToString(Number), 2);
         }
-        public int DecimalToOctal() {
-
+        public string DecimalToOctal() {
+            return Convert.ToString(Number, 8);
         }
         public int OctalToDecimal() {
-
+            return Convert.ToInt32(Convert.ToString(Number), 8);
         }
-        public int DecimalToHex() {
-
+        public string DecimalToHex() {
+            return Convert.ToString(Number, 16);
         }
         public int HexToDecimal() {
-
+            return Convert.ToInt32(Convert.ToString(Number), 16);
         }
-        public int BinaryToOctal() {
-
+        public string BinaryToOctal() {
+            return Convert.ToString(BinaryToDecimal(), 8);
         }
-        public int OctalToBinary() {
-
+        public string OctalToBinary() {
+            return Convert.ToString(OctalToDecimal(), 2);
         }
-        public int BinaryToHex() {
-
+        public string BinaryToHex() {
+            return Convert.ToString(BinaryToDecimal(), 16);
         }
-        public int HexToBinary() {
-
+        public string HexToBinary() {
+            return Convert.ToString(HexToDecimal(), 2);
         }
-        public int OctalToHex() {
-
+        public string OctalToHex() {
+            return Convert.ToString(OctalToDecimal(), 16);
         }
-        public int HexToOctal() {
-
+        public string HexToOctal() {
+            return Convert.ToString(HexToDecimal(), 8);
         }
         public void PrintResult() {
-
+            switch(FromBase + "TO" + ToBase) {
+                case "decimalTObinary":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, DecimalToBinary(), ToBase);
+                    break;
+                case "binaryTOdecimal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, BinaryToDecimal(), ToBase);
+                    break;
+                case "decimalTOoctal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, DecimalToOctal(), ToBase);
+                    break;
+                case "octalTOdecimal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, OctalToDecimal(), ToBase);
+                    break;
+                case "decimalTOhex":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, DecimalToHex(), ToBase);
+                    break;
+                case "hexTOdecimal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, HexToDecimal(), ToBase);
+                    break;
+                case "binaryTOoctal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, BinaryToOctal(), ToBase);
+                    break;
+                case "octalTObinary":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, OctalToBinary(), ToBase);
+                    break;
+                case "binaryTOhex":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, BinaryToHex(), ToBase);
+                    break;
+                case "hexTObinary":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, HexToBinary(), ToBase);
+                    break;
+                case "octalTOhex":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, OctalToHex(), ToBase);
+                    break;
+                case "hexTOoctal":
+                    Console.WriteLine("{1} in {2} is {3} in {4}. ", Number, FromBase, HexToOctal(), ToBase);
+                    break;
+                default: 
+                    Console.WriteLine("This program only support the conversion between 2, 8, 10 and 16. ");    
+                    break;
+            }
         }       
     }
 }
